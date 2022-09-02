7d65b47f711a5af85d846b06f3bbb5f17166b6c1
Adam Mroz <MrozAdam1992@gmail.com>
2022-09-02 21:06:05 +0200

    Initial Commit

---
+++ AdamCard/AdamCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdamCardApp.swift
+//  AdamCard
+//
+//  Created by Adam Mroz on 02/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct AdamCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
